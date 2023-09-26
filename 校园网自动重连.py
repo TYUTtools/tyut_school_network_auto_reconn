cac22f1f087d7f1dfe16eb76fb7e5144bb0f6a8c
赵苦瓜 <1066168689@qq.com>
2023-09-26 02:49:01 +0000

    update 校园网自动重连.py.
    Signed-off-by: 赵苦瓜 <1066168689@qq.com>

--- 校园网自动重连.py
+++ 校园网自动重连.py
@@ -3,6 +3,19 @@ import requests
 from time import sleep
 from datetime import datetime
 
+user_account = 'xiaoming5567'  # 校园网用户名，通常为姓名拼音加上学号后四位
+user_password = 'MDij0765'  # 校园网密码，通常由两个大写字母+两个小写字母+四位数字组成
+
+
+def get_local_ip():
+    import socket
+
+    # 获取本机计算机名称
+    hostname = socket.gethostname()
+    # 获取本机ip
+    ip = socket.gethostbyname(hostname)
+    return ip
+
 
 def xiaoyuanwang_test():
     try:
@@ -12,21 +25,24 @@ def xiaoyuanwang_test():
     except BaseException as e:
         print(e)
         print('连接断开！正在尝试重连...')
-        # login_url = "http://219.226.127.250:801/eportal/?c=ACSetting&a=Login&protocol=http:&hostname=219.226.127.250&port=&iTermType=1&wlanuserip=&wlanacip=&wlanacname=&redirect=&session=&ssid=&vlanid=&queryACIP=0&jsVersion=1.3.8&isTelephone=0&DDDDD=,0,【校园网用户名】&upass=【校园网密码】&R1=0&R2=&R6=0&v6ip=&para=00&0MKKey=123456"
-        login_url = "http://219.226.127.250:801/eportal/portal/login?callback=dr1003&login_method=1&user_account=%2C0%2C【校园网用户名】&user_password=【校园网密码】&wlan_user_ip=101.7.172.6&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.1.3&terminal_type=1&lang=zh-cn&v=8262&lang=zh"
+
+        login_url = f"http://219.226.127.250:801/eportal/portal/login?callback=dr1003&login_method=1&user_account=%2C0%2C{user_account}&user_password={user_password}&wlan_user_ip={get_local_ip()}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.1.3&terminal_type=1&lang=zh-cn&v=4005&lang=zh"
+
         try:
-            requests.get(login_url)
+            res1 = requests.get(login_url)
             res = requests.get('https://baidu.com')
             print('重连成功！')
             return True
         except BaseException as e:
-            print('重连失败！')
+            print('重连失败！', res1.text)
             print(e)
             return False
     return True
 
+
 if __name__ == '__main__':
     count=0
+    print(get_local_ip())
     while 1:
         flag = xiaoyuanwang_test()
         if flag:
